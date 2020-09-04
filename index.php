676bcfbf47fb4d91d4422115ce20bbc86cf21b8b
adlan.sadou <adlsadou@gmail.com>
2020-09-04 18:51:06 +0200

    +added: mySql database connection

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+        <h1>Your To-Do list!</h1>
+    </div>
+    <form action="input.php" method="POST">
+
+    </form>
+    <?php 
+        echo "<h1>Hello PhP!</h1>";
+        include 'input.php';
+    ?>
+</body>
+</html>
